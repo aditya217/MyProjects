866e3e24c9407e9e8ee052d86e25597f89846708
adi-tya-1994 <itsmeaadi3@gmail.com>
2021-08-03 22:21:26 +0530

    My-Project

---
+++ scripts/main.js
@@ -0,0 +1,19 @@
+let userName = prompt("Enter Your Name");
+document.getElementById('userName').textContent = 'Welcome '+userName;
+const myImage = document.querySelector('img');
+let flag = false;
+myImage.onclick = function(){
+    flag = !flag;
+    if(flag)
+    {
+        myImage.src = 'images/firefox_logo2.jpg';
+        let myHeader = document.querySelector('h1');
+        myHeader.textContent = "Chrome is Awesome ";    
+    }
+    else
+    {
+        myImage.src = 'images/firefox_logo.jpg';
+        let myHeader = document.querySelector('h1');
+        myHeader.textContent = "Mozilla is Awesome";    
+    }
+}
